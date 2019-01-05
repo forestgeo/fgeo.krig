5d4782f7575b10e04fac78a175145676028f0f28
maurolepore <maurolepore@gmail.com>
2019-01-05 07:37:07 -0500

    Prune tests.

--- tests/testthat/test-to_df.R
+++ tests/testthat/test-to_df.R
@@ -1,26 +1,13 @@
 context("to_df.krig_lst")
 
-vars <- c("c", "p")
-out_lst <- fgeo.krig::krig(fgeo.krig::soil_fake, vars, quiet = TRUE)
-out_df <- to_df(out_lst)
-
-test_that("passes silently with data of correct class", {
-  expect_silent(head(out_df))
-})
-
-test_that("fails with unknown class", {
-  expect_error(to_df(character(1)), "Can't deal with data of class")
+test_that("to_df.krig_lst fails with unknown class", {
   expect_error(to_df(data.frame(1)), "Can't deal with data of class")
   expect_error(to_df(list(1)), "Can't deal with data of class")
-
-  expect_error(to_df(out_lst, name = 1))
-  expect_error(to_df(out_lst, item = 1))
-  expect_error(to_df(out_lst, item = "bad_item"))
-  expect_error(to_df(out_lst, item = c("df", "df.poly")))
 })
 
-test_that("outputs an object of the expected class", {
-  # no-longer class krig_lst
+test_that("to_df.krig_lst returns a tibble/dataframe", {
+  out_lst <- krig(fgeo.krig::soil_fake, c("c", "p"), quiet = TRUE)
+
   expect_false(any("krig_lst" %in% class(to_df(out_lst))))
   expect_is(to_df(out_lst), "data.frame")
   expect_is(to_df(out_lst), "tbl")
